094d9c7c5d79fe92e92ffd4c9feef1a92c5dbfda
jishanahmad <jishan.ahmad1992@gmail.com>
2022-04-29 18:14:56 +0530

    added new files

--- 2.OOPS/src/oops/inheritance_4.scala
+++ 2.OOPS/src/oops/inheritance_4.scala
@@ -4,20 +4,15 @@ object inheritance_4 {
 
   def main(args: Array[String]): Unit = {
 
-    class Animal{
+    sealed class Animal{
       def eat = println("From animal class")
     }
 
     class cat extends Animal
 
-    class Dog extends Animal{
-      override def eat = {
-        super.eat
-        println("Eat,eat")
-      }
+    class Dog extends Animal {
+      override def eat = println("Eat,eat")
     }
 
-    val unknownAnimal: Animal = new Dog
-    unknownAnimal.eat
   }
 }
