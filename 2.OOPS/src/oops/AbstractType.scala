6a5af65a39e5557fa4e82003510effcb921845b9
jishanahmad <jishan.ahmad1992@gmail.com>
2022-04-29 21:58:23 +0530

    modified readme

--- 2.OOPS/src/oops/AbstractType.scala
+++ 2.OOPS/src/oops/AbstractType.scala
@@ -1,18 +1,37 @@
 package oops
 
-object inheritance_4 {
+abstract class MyList{
+  def head: Int
+  def tail: MyList
+  def isEmpty : Boolean
+  def add(elements: Int): MyList
+  def printElements: String
+  override def toString: String = "[" + printElements + "]"
+}
 
-  def main(args: Array[String]): Unit = {
+object Empty extends MyList{
+  def head: Int = throw new NoSuchElementException
+  def tail: MyList = throw new NoSuchElementException
+  def isEmpty : Boolean = true
+  def add(elements: Int): MyList = new Cons(elements, Empty)
+  override def toString: String = ""
+}
 
-    sealed class Animal{
-      def eat = println("From animal class")
-    }
-
-    class cat extends Animal
-
-    class Dog extends Animal {
-      override def eat = println("Eat,eat")
-    }
-
+class Cons(h:Int, t:MyList) extends  MyList{
+  def head: Int = h
+  def tail: MyList = t
+  def isEmpty : Boolean = false
+  def add(elements: Int): MyList = new Cons(elements, this)
+  def printElements: String = {
+    if(t.isEmpty) "" + h
+    else s"$h ${t.printElements}"
   }
+}
+
+object MyList extends App{
+  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  println(list.tail.head)
+  println(list.add(4).head)
+  println(list.isEmpty)
+  println(list.toString)
 }
